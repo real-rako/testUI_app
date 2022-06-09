cc3c9085c1d0ad177fc3a0012449ae553eb75217
David Meester <rakodavid1@gmail.com>
2022-06-10 01:19:19 +0200

    Initial Commit

---
+++ TestUI-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestUI-app
+//
+//  Created by David Meester on 10/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestUI-app/TestUI_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUI_appApp.swift
+//  TestUI-app
+//
+//  Created by David Meester on 10/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestUI_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
