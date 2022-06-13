c3c64439bf0bc9e78b44f223d0271c2718d52296
David Meester <rakodavid1@gmail.com>
2022-06-13 16:30:43 +0200

    Added some really basic functionality to the app. You can now change the view of the card by clicking on it!

--- TestUI-app/ContentView.swift
+++ TestUI-app/ContentView.swift
@@ -7,15 +7,54 @@
 
 import SwiftUI
 
+// ContentView - main
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            CardView()
+            CardView(IsUp: false)
+            CardView()
+             CardView(IsUp: false)
+        }.padding(.horizontal)
     }
 }
+
+
+
+struct CardView: View {
+    @State var IsUp: Bool = true;
+    var body: some View {
+        ZStack {
+            let figure  = RoundedRectangle(cornerRadius: 25)
+
+            if IsUp {
+            figure.stroke(lineWidth: 4)
+            figure.fill(.white)
+            
+                Text("🏎").font(.largeTitle)}
+            else {
+                figure.fill(.blue)
+            }
+        }.foregroundColor(.blue)
+            .onTapGesture {
+                if IsUp {
+                    IsUp = false
+                } else {IsUp = true}
+            }
+    }
+}
+ 
+
+// ContentView_Previews
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
+        
+        ContentView()
+            .preferredColorScheme(.dark)
+            .previewInterfaceOrientation(.portrait)
     }
 }
