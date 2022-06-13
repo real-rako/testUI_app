b60feed4072f5cdc3b590b848ac597c599e6bffa
David Meester <rakodavid1@gmail.com>
2022-06-14 01:18:26 +0200

    Added some basic grid-alignment

--- TestUI-app/ContentView.swift
+++ TestUI-app/ContentView.swift
@@ -5,42 +5,81 @@
 //  Created by David Meester on 10/06/2022.
 //
 
-import SwiftUI
+import SwiftUI;
 
 // ContentView - main
 
 struct ContentView: View {
+    var Emotes = ["🚗","🚕","🏎","🚓","🚡","✈️"];
+    @State var EmoteCount = 4
+    let columns = [GridItem(),GridItem(),GridItem()]
     var body: some View {
-        HStack {
-            CardView()
-            CardView(IsUp: false)
-            CardView()
-             CardView(IsUp: false)
-        }.padding(.horizontal)
+        VStack {
+            LazyVGrid(columns: columns) {
+                ForEach(Emotes[0..<EmoteCount],id: \.self) { emoji in
+                    CardView(content: emoji).aspectRatio(2/3, contentMode: .fit);
+                }
+            }
+            .padding(.horizontal);
+            Spacer()
+            HStack {
+                MinButton
+                Spacer()
+                PlusButton
+            }
+        }
+        
     }
+    var MinButton: some View {
+        Button(action: {
+            if EmoteCount > 1 {
+                EmoteCount -= 1
+            }
+            else {
+                // give error to user
+            }
+        }, label: {
+            Image(systemName: "minus.circle")
+        }).padding(.horizontal).font(.largeTitle)
+
+    }
+    var PlusButton: some View {
+        Button(action: {
+            if EmoteCount < Emotes.count {
+                EmoteCount += 1
+            }
+            else {
+                // give error to user
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        }).padding(.horizontal).font(.largeTitle)
+        
+    }
+
+
 }
 
 
 
 struct CardView: View {
-    @State var IsUp: Bool = true;
+    var content: String
+    @State var IsUp: Bool = false;
     var body: some View {
         ZStack {
             let figure  = RoundedRectangle(cornerRadius: 25)
 
             if IsUp {
-            figure.stroke(lineWidth: 4)
-            figure.fill(.white)
+                figure.stroke(lineWidth: 4)
+                figure.fill(.white)
             
-                Text("🏎").font(.largeTitle)}
+                Text(content).font(.largeTitle)}
             else {
-                figure.fill(.blue)
+                figure.fill(.red)
             }
         }.foregroundColor(.blue)
             .onTapGesture {
-                if IsUp {
-                    IsUp = false
-                } else {IsUp = true}
+                IsUp = !IsUp
             }
     }
 }
@@ -53,8 +92,8 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
             .previewInterfaceOrientation(.portrait)
         
-        ContentView()
-            .preferredColorScheme(.dark)
-            .previewInterfaceOrientation(.portrait)
+//        ContentView()
+//            .preferredColorScheme(.dark)
+//            .previewInterfaceOrientation(.portrait)
     }
 }
